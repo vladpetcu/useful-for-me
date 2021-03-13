47e2d6aaa495f9d986fb5498dc0d8696bd2e76d7
vladpetcu <petcuvlad92@gmail.com>
2021-03-13 12:39:30 +0200

    Added vs code extensions file

---
+++ install-vs-code-extensions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo code --install-extension eamodio.gitlens --user-data-dir="~/.vscode-root"
+
+sudo code --install-extension PKief.material-icon-theme --user-data-dir="~/.vscode-root"
+sudo code --install-extension esbenp.prettier-vscode --user-data-dir="~/.vscode-root"
+sudo code --install-extension michelemelluso.code-beautifier --user-data-dir="~/.vscode-root"
+sudo code --install-extension streetsidesoftware.code-spell-checker --user-data-dir="~/.vscode-root"
+sudo code --install-extension BriteSnow.vscode-toggle-quotes --user-data-dir="~/.vscode-root"
+sudo code --install-extension andys8.jest-snippets --user-data-dir="~/.vscode-root"
+sudo code --install-extension dbaeumer.vscode-eslint --user-data-dir="~/.vscode-root"
+sudo code --install-extension ms-python.vscode-pylance --user-data-dir="~/.vscode-root"
+sudo code --install-extension ms-python.python --user-data-dir="~/.vscode-root"
