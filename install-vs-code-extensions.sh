a2153b1d698a09ac7cb11d0cf794cacceab77d67
vladpetcu <petcuvlad92@gmail.com>
2021-10-23 14:21:31 +0300

    Updated vs-code installer script

--- install-vs-code-extensions.sh
+++ install-vs-code-extensions.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-declare -a user_dirs=("~/.vscode-root" "~/.vscode")
+declare -a extensions=(
+        "eamodio.gitlens"
+        "PKief.material-icon-theme"
+        "esbenp.prettier-vscode"
+        "michelemelluso.code-beautifier"
+        "streetsidesoftware.code-spell-checker"
+        "BriteSnow.vscode-toggle-quotes"
+        "andys8.jest-snippets"
+        "dbaeumer.vscode-eslint"
+        "ms-python.vscode-pylance"
+        "ms-python.python"
+        "ms-toolsai.jupyter"
+    )
 
-for user_dir in "${user_dirs[@]}"
+for extension in "${extensions[@]}"
 do
-    sudo code --install-extension eamodio.gitlens --force --user-data-dir="$user_dir"
-
-    sudo code --install-extension PKief.material-icon-theme --force --user-data-dir="$user_dir"
-    sudo code --install-extension esbenp.prettier-vscode --force --user-data-dir="$user_dir"
-    sudo code --install-extension michelemelluso.code-beautifier --force --user-data-dir="$user_dir"
-    sudo code --install-extension streetsidesoftware.code-spell-checker --force --user-data-dir="$user_dir"
-    sudo code --install-extension BriteSnow.vscode-toggle-quotes --force --user-data-dir="$user_dir"
-    sudo code --install-extension andys8.jest-snippets --force --user-data-dir="$user_dir"
-    sudo code --install-extension dbaeumer.vscode-eslint --force --user-data-dir="$user_dir"
-    sudo code --install-extension ms-python.vscode-pylance --force --user-data-dir="$user_dir"
-    sudo code --install-extension ms-python.python --force --user-data-dir="$user_dir"
-    sudo code --install-extension ms-toolsai.jupyter --force --user-data-dir="$user_dir"
+    sudo code --install-extension "$extension" --force --user-data-dir="~/.vscode-root"
+    code --install-extension "$extension" --force
 done
 
