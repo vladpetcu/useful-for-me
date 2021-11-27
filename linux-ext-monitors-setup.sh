2f4ea0edaf567b5341e2c1bdeab56b3effed5ddb
Vlad Petcu <petcuvlad92@gmail.com>
2021-11-27 21:00:48 +0200

    exteranl monitors script updates

--- linux-ext-monitors-setup.sh
+++ linux-ext-monitors-setup.sh
@@ -7,14 +7,13 @@
 # default monitor is eDP-1
 MONITOR=eDP1
 
+
 # functions to switch from eDP-1 to EXTERNAL and vice versa
 function ActivateEXTERNAL {
-    echo "Switching to EXTERNAL"
     xrandr --output DVI-I-3-2 --output DVI-I-2-1 --output eDP-1 --same-as DVI-I-2-1
     MONITOR=EXTERNAL
 }
 function DeactivateEXTERNAL {
-    echo "Switching to eDP-1"
     xrandr --output DVI-I-3-2 --off --output DVI-I-2-1 --off --output eDP-1 --auto 
     MONITOR=eDP1
 }
@@ -27,19 +26,15 @@ function EXTERNALConnected {
     [ $(xrandr | grep "DVI-I-2-1\|DVI-I-3-2" | grep disconnected | wc -l) = "0" ]
 }
 
-# actual script
-while true
-do
-    if ! EXTERNALActive && EXTERNALConnected; then
-        ActivateEXTERNAL
-    fi
+# actual script, do not run it in a infinite loop
 
-    if EXTERNALActive && ! EXTERNALConnected; then
-        DeactivateEXTERNAL
-    fi
+if ! EXTERNALActive && EXTERNALConnected; then
+    ActivateEXTERNAL
+fi
 
-    sleep 1s
-done
+if EXTERNALActive && ! EXTERNALConnected; then
+    DeactivateEXTERNAL
+fi
 
 ext=$?
 if [[ $ext -ne 0 ]]; then
